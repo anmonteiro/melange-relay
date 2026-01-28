e52fc68b890142f2be465d5c8bc7e13c5fb8bcb3
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-01-27 23:07:50 -0800

    update deps

--- flake.nix
+++ flake.nix
@@ -3,61 +3,83 @@
 
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
+  inputs.nixpkgs = {
+    # url = "github:nix-ocaml/nix-overlays";
+    url = "/Users/anmonteiro/monorepo/nix-overlays";
+    inputs.flake-utils.follows = "flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nix-filter,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages."${system}".appendOverlays [
           (self: super: {
-            ocamlPackages = super.ocaml-ng.ocamlPackages_5_2;
+            ocamlPackages = super.ocaml-ng.ocamlPackages_5_3;
           })
         ];
-        inherit (pkgs) nodejs_latest lib stdenv darwin;
+        inherit (pkgs)
+          nodejs_latest
+          lib
+          stdenv
+          ;
       in
       rec {
         packages = {
-          default = with pkgs.ocamlPackages; buildDunePackage {
-            pname = "melange-relay";
-            version = "dev";
+          default =
+            with pkgs.ocamlPackages;
+            buildDunePackage {
+              pname = "melange-relay";
+              version = "dev";
 
-            src = ./.;
-            nativeBuildInputs = [
-              pkgs.ocamlPackages.melange
-              reason
-            ];
-            nativeCheckInputs = [ nodejs_latest ];
-            checkInputs = [ melange-fetch nodejs_latest ];
-            doCheck = true;
-            propagatedBuildInputs = [
-              pkgs.ocamlPackages.melange
-              reason-react
-              reason-react-ppx
-              graphql_parser
-            ];
-          };
+              src = ./.;
+              nativeBuildInputs = [
+                pkgs.ocamlPackages.melange
+                reason
+              ];
+              nativeCheckInputs = [ nodejs_latest ];
+              checkInputs = [
+                melange-fetch
+                nodejs_latest
+              ];
+              doCheck = true;
+              propagatedBuildInputs = [
+                melange
+                reason-react
+                reason-react-ppx
+                graphql_parser
+              ];
+            };
         };
         defaultPackage = packages.native.piaf;
         devShells.default = pkgs.mkShell {
           inputsFrom = [ packages.default ];
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            yarn
-            cargo
-            nodejs_latest
-            openssl-oc
-            rustfmt
-          ] ++ (with pkgs.ocamlPackages; [
-            ocamlformat
-            merlin
-          ]);
+          nativeBuildInputs =
+            with pkgs;
+            [
+              pkg-config
+              yarn
+              cargo
+              nodejs_latest
+              rustfmt
+            ]
+            ++ (with pkgs.ocamlPackages; [
+              ocamlformat
+              merlin
+            ]);
           propagatedBuildInputs = lib.optionals stdenv.isDarwin [
             pkgs.libiconv
-            darwin.apple_sdk.frameworks.Security
           ];
           shellHook = ''
             PATH=$PATH:$PWD/melange-relay-compiler/compiler/target/debug
           '';
         };
-      });
+      }
+    );
 }
